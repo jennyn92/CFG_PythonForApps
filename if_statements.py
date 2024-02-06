e9a5215cbecc1355ee8497715551bb99b13b81a0
jennyn92 <134844026+jennyn92@users.noreply.github.com>
2024-02-06 19:27:27 +0000

    1st commit

---
+++ if_statements.py
@@ -0,0 +1,18 @@
+#password exercise
+
+#password = input('password: ')
+
+#if password == "jumanji" :
+   # print('Success!')
+
+name =input("What is your name?: ")
+is_admin = name == "admin"
+
+password =input("What is your password?: ")
+is_password_correct = password == "dinosaurs"
+
+if is_admin and is_password_correct:
+    print("Welcome")
+
+if not is_admin or not is_password_correct:
+    print('Go away')
