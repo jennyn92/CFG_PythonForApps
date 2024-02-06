e19dd7ca3d941d8aa84f8035aafe197d38ac61a6
jennyn92 <134844026+jennyn92@users.noreply.github.com>
2024-02-06 19:59:45 +0000

    update

--- Lesson_3.py
+++ Lesson_3.py
@@ -11,52 +11,42 @@ Probably not a good idea
 '''
 
 
-burger_price= input("How much is the burger?")
 
-veg_option =input("Is there a vegetarian option? y/n: " )
-
-
-budget = float(burger_price) <=10
-veg = veg_option == "y"
-
-is_good_choice = budget and veg
-
-if is_good_choice == True:
-    print("This restaurant is a great choice")
-
-if not is_good_choice:
-    print("Probably not a good idea")
+# burger_price= input("How much is the burger?")
+# veg_option =input("Is there a vegetarian option? y/n: " )
+#
+# budget = float(burger_price) <=10
+# veg = veg_option == "y"
+#
+# is_good_choice = budget and veg
+#
+# if is_good_choice == True:
+#     print("This restaurant is a great choice")
+# if not is_good_choice:
+#     print("Probably not a good idea")
 
 #Calculate price of meal and discount
 
 meal_cost = float(input("How much was your meal?"))
 
-
-
 if meal_cost <=20:
     print (f"No discount applied, Your meal is £{meal_cost}")
-
 else:
     print (f"Discount applied, Your meal is £ {meal_cost * 0.9}")
-
-
-
 
 
 #print(f'Your burger is within the budget: {budget} and is vegetarian = {veg_option}')
 #print('Restaurant meets criteria: {}'.format(is_good_choice))
 
-
-
 #new_exercise
-'''
-mars_choice= input('Would you like to visit Mars? y/n: ')
-is_willing = mars_choice == 'y'
 
-affordable =input('Can you afford to visit Mars y/n: ')
-can_afford = affordable == 'y'
-
-should_visit_mars = is_willing and can_afford
-
-print(f'You should visit Mars: {should_visit_mars}')
-'''
+# mars_choice= input('Would you like to visit Mars? y/n: ')
+# is_willing = mars_choice == 'y'
+#
+# affordable =input('Can you afford to visit Mars y/n: ')
+# can_afford = affordable == 'y'
+#
+# should_visit_mars = is_willing and can_afford
+#
+# print(f'You should visit Mars: {should_visit_mars}')
+#
--- if_statements.py
+++ if_statements.py
@@ -5,14 +5,44 @@
 #if password == "jumanji" :
    # print('Success!')
 
-name =input("What is your name?: ")
-is_admin = name == "admin"
+# name =input("What is your name?: ")
+# is_admin = name == "admin"
+#
+# password =input("What is your password?: ")
+# is_password_correct = password == "dinosaurs"
+#
+# if is_admin and is_password_correct:
+#     print("Welcome")
+#
+# else:
+#     print('Go away')
 
-password =input("What is your password?: ")
-is_password_correct = password == "dinosaurs"
+#elif
+# dog_size =int(input('How big is the dog?'))
+#
+# if dog_size > 75:
+#     print("It's a big dog")
+#
+# elif dog_size <10:
+#     print("Super small dog")
+#
+# elif dog_size <25:
+#     print("That is a small dog")
+#
+# else:
+#     print("That is an average dog")
 
-if is_admin and is_password_correct:
-    print("Welcome")
+#Pizza Exerise
+#oven temp needs to be correct
 
+#ask user input temp
+temp = int(input("What temperature is oven set at?"))
+
+if temp > 200:
+    print("The oven is too hot")
+elif temp ==180:
+    print("The oven is at the perfect temperature")
+elif temp < 150:
+    print("The oven is too cold")
 else:
-    print('Go away')
+    print("The temp is close enough.")
